eb8ec71db241c1e1629a922976584f523d23263f
Abey John Pramod <146655666+abeyjohnp@users.noreply.github.com>
2026-06-09 16:45:00 +0530

    removed comment signs

--- Contest-1/section3.js
+++ Contest-1/section3.js
@@ -1,81 +1,81 @@
 const fs = require("fs")
 //1. User Email Extractor
 
-    // let emails = []
-    // fs.readFile("users.json","utf-8", function(err,data){
-    //     let newobj = JSON.parse(data);
-    //     //console.log(newobj)
+    let emails = []
+    fs.readFile("users.json","utf-8", function(err,data){
+        let newobj = JSON.parse(data);
+        //console.log(newobj)
 
-    //     for (let i =0 ; i < newobj.length ; i++)
-    //     {
-    //         emails.push(newobj[i].email)
-    //     }
-    //     console.log(emails)
-    //     emails=emails.join('\n')
-    //     fs.writeFile("emails.txt",emails,function(err,data){
+        for (let i =0 ; i < newobj.length ; i++)
+        {
+            emails.push(newobj[i].email)
+        }
+        console.log(emails)
+        emails=emails.join('\n')
+        fs.writeFile("emails.txt",emails,function(err,data){
 
-    //         })
-    //     }
-    // )
+            })
+        }
+    )
 
 //2. Student Marks Report Generator
 
-    // let marks = []
-    // fs.readFile("marks.json","utf-8",function(err,data){
-    //     let newobj = JSON.parse(data);
-    //     for (let i =0 ; i < newobj.length ; i++)
-    //     {
-    //         marks.push(parseInt(newobj[i].marks))
-    //     }
-    //     for (let i=0 ; i<marks.length ; i++)
-    //     {
-    //         for (let j=0 ; j<marks.length ; j++)
-    //         {
-    //             if (marks[j]<marks[i])
-    //             {
-    //                 let temp = marks[j];
-    //                 marks[j] = marks[i];
-    //                 marks[i] = temp;
-    //             }
-    //         }
-    //     }
-    //     console.log(marks)
-    //     record = "Highest : "+marks[1]+"\nAverage : "+((marks[0]+marks[1]+marks[2])/3)+"\nLowest : "+marks[2];
-    //     fs.writeFile("report.txt",record,function(err,data){
+    let marks = []
+    fs.readFile("marks.json","utf-8",function(err,data){
+        let newobj = JSON.parse(data);
+        for (let i =0 ; i < newobj.length ; i++)
+        {
+            marks.push(parseInt(newobj[i].marks))
+        }
+        for (let i=0 ; i<marks.length ; i++)
+        {
+            for (let j=0 ; j<marks.length ; j++)
+            {
+                if (marks[j]<marks[i])
+                {
+                    let temp = marks[j];
+                    marks[j] = marks[i];
+                    marks[i] = temp;
+                }
+            }
+        }
+        console.log(marks)
+        record = "Highest : "+marks[1]+"\nAverage : "+((marks[0]+marks[1]+marks[2])/3)+"\nLowest : "+marks[2];
+        fs.writeFile("report.txt",record,function(err,data){
 
-    //     })
-    // })
+        })
+    })
 
 
 //3. Notification Service Simulator
 
-    // fs.readFile("events.txt","utf-8",function(err,data){
-    //     //console.log(data)
-    //     let newdata= data.split('\r\n');
-    //     //console.log(newdata)
-    //     let uctr=0
-    //     let op = 0
-    //     let pr =0
-    //     for (let i=0; i< newdata.length ; i++)
-    //     {
-    //        if (newdata[i]=="USER_SIGNUP")
-    //        {
-    //             uctr++
-    //        }
-    //        else if (newdata[i]=="ORDER_PLACED")
-    //        {
-    //             op++
-    //        }
-    //        else
-    //        {
-    //             pr++
-    //        }
-    //     }
-    //     string = "USER_SIGNUP : "+uctr+"\nORDER_PLACED : "+op+"\nPASSWORD_RESET : "+pr
-    //     fs.writeFile("analytics.txt",string,function(err,data){
+    fs.readFile("events.txt","utf-8",function(err,data){
+        //console.log(data)
+        let newdata= data.split('\r\n');
+        //console.log(newdata)
+        let uctr=0
+        let op = 0
+        let pr =0
+        for (let i=0; i< newdata.length ; i++)
+        {
+           if (newdata[i]=="USER_SIGNUP")
+           {
+                uctr++
+           }
+           else if (newdata[i]=="ORDER_PLACED")
+           {
+                op++
+           }
+           else
+           {
+                pr++
+           }
+        }
+        string = "USER_SIGNUP : "+uctr+"\nORDER_PLACED : "+op+"\nPASSWORD_RESET : "+pr
+        fs.writeFile("analytics.txt",string,function(err,data){
 
-    //     })
-    // })
+        })
+    })
 
 
 
