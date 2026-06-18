76ba9e24edb699d6504129246f4d369c337cd1e2
Abey John Pramod <146655666+abeyjohnp@users.noreply.github.com>
2026-06-18 12:21:05 +0530

    POST routes for SIGNUP AND SIGNIN established

---
+++ Contest-5 (CRUD)/backend/index.js
@@ -0,0 +1,138 @@
+const express = require("express")
+const app = express()
+const {z, success} = require("zod")
+app.use(express.json())
+let cntr = 0
+const db = {
+  users: [],
+  movies: [
+    {
+      id:1,
+      title:"Inception",
+      genre:"Sci-Fi",
+      duration:148,
+      shows: [
+        {
+          showId:101,
+          time:"10:00 AM",
+          pricePerSeat:200,
+          availableSeats:50
+        },
+        {
+          showId:102,
+          time:"2:00 PM",
+          pricePerSeat:250,
+          availableSeats:50
+        },
+        {
+          showId:103,
+          time:"6:00 PM",
+          pricePerSeat:300,
+          availableSeats:50
+        }
+      ]
+    },
+    {
+      id:2,
+      title:"The Dark Knight",
+      genre:"Action",
+      duration:152,
+      shows: [
+        {
+          showId:201,
+          time:"11:00 AM",
+          pricePerSeat:200,
+          availableSeats:50
+        },
+        {
+          showId:202,
+          time:"3:00 PM",
+          pricePerSeat:250,
+          availableSeats:50
+        },
+        {
+          showId:203,
+          time:"7:00 PM",
+          pricePerSeat:300,
+          availableSeats:50
+        }
+      ]
+    },
+    {
+      id:3,
+      title:"Interstellar",
+      genre:"Sci-Fi",
+      duration:169,
+      shows: [
+        {
+          showId:301,
+          time:"12:00 PM",
+          pricePerSeat:250,
+          availableSeats:50
+        },
+        {
+          showId:302,
+          time:"5:00 PM",
+          pricePerSeat:300,
+          availableSeats:50
+        }
+      ]
+    }
+  ]
+}
+const userNameSchema = z.object({
+    username : z.string().max(10),
+    password : z.string()
+        .min(4)
+        .max(10),
+    email : z.email("Invalid syntax")
+})
+app.post("/signup",(req,res) => {
+    const result = userNameSchema.safeParse(req.body)
+    if (!result.success)
+    {
+        return res.status(400).json({
+            success : false,
+            message : "The password does not meet the requirements"
+        })
+    }
+    const {username} = result.data
+    const {password} = result.data
+    const {email} = result.data
+    res.status(201).json({
+        success : true,
+        message : "User has successfully logged in",
+        userId : cntr
+    })
+
+    db.users.push(
+        {
+            id:cntr++,
+            username:username,
+            password:password,
+            email:email,
+            
+        }
+    )
+    console.log(db.users)
+})
+app.post("/signin",(req,res) =>{
+    let token = Math.random().toString()
+    let username = req.body.username
+    let password =  req.body.password
+
+    let index = db.users.findIndex(user => 
+        (user.username === username && user.password === password))
+    
+    res.status(200).json({
+        success : true,
+        message : "Signin successful",
+        token : token
+    })
+
+    
+})
+
+app.listen(3000,()=>{
+    console.log("Server is listening on http://localhost:3000")
+})
